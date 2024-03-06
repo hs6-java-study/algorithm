d13c5502ec46ed50011eddee5a824068ab84306a
alsth712 <alsth712@naver.com>
2024-03-06 23:41:46 +0900

    feat: 알고리즘 2일차 과제 제출

--- 1주차/20240305/sm_10998.java
+++ 1주차/20240305/sm_10998.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        int sum = a * b;
-        System.out.println(sum);
+        String name = sc.nextLine();
+        System.out.println(name + "??!");
         sc.close();
     }
 }
