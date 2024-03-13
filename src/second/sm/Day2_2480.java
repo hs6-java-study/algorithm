29d621a87b11880cbaa27d073dc42ef628533221
unknown <alsth712@naver.com>
2024-03-13 19:42:01 +0900

    feat: 2일차 알고리즘 완료

---
+++ src/second/sm/Day2_2480.java
@@ -0,0 +1,28 @@
+package second.sm;
+
+import java.util.Scanner;
+
+public class Day2_2480 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int result = 0;
+        int selectedNum = 0;
+
+        if(a == b && b == c) {
+            result = 10000+a*1000;
+        } else if(a == b || b == c || a == c) {
+            selectedNum = a == b ? a : b == c ? b : a;
+            result = 1000+selectedNum*100;
+        } else {
+            if(a>b && a>c) selectedNum = a;
+            else if (b>a && b>c) selectedNum = b;
+            else selectedNum = c;
+
+            result = selectedNum * 100;
+        }
+        System.out.println(result);
+    }
+}
