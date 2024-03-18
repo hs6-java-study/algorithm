03efa16c7530d25e6f5598abf15225615a30f389
Somin Lee <81960250+purin48@users.noreply.github.com>
2024-03-19 08:49:42 +0900

    feat: 알고리즘 5일차 과제 제출 (#24)

---
+++ src/second/sm/Day5_10807.java
@@ -0,0 +1,20 @@
+package second.sm;
+
+import java.util.Scanner;
+
+public class Day5_10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int findNum = sc.nextInt();
+        int cnt = 0;
+        for(int val : arr) {
+            if(val == findNum) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
